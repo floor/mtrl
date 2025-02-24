4d374cca0d3b9e8c85f04651f432ddcc5cb5aa46
Jerome Vial <jerome.vial@gmail.com>
2025-02-24 23:26:50 +0100

    add menu component

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // index.js
 import {
   createLayout,
-  createElement, createButton, createCheckbox, createTextfield, createSwitch, createContainer, createList, createSnackbar, createNavigation
+  createElement,
+  createButton, createCheckbox, createTextfield, createSwitch, createContainer, createList, createSnackbar, createNavigation, createMenu
 } from './src/index.js'
 
 export {
   createLayout,
-  createElement, createButton, createCheckbox, createTextfield, createSwitch, createContainer, createList, createSnackbar, createNavigation
+  createElement, createButton, createCheckbox, createTextfield, createSwitch, createContainer, createList, createSnackbar, createNavigation, createMenu
 }
