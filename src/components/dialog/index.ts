935755eb71d9dea683feeb9e49105a5379634de9
DrJones <drjones@floor.io>
2025-03-02 18:34:23 +0100

    feature: add dialog component

---
+++ src/components/dialog/index.ts
@@ -0,0 +1,15 @@
+// src/components/dialog/index.ts
+export { default } from './dialog';
+export { 
+  DIALOG_SIZES, 
+  DIALOG_ANIMATIONS, 
+  DIALOG_FOOTER_ALIGNMENTS, 
+  DIALOG_EVENTS 
+} from './constants';
+export { 
+  DialogConfig, 
+  DialogComponent, 
+  DialogButton, 
+  DialogEvent, 
+  DialogConfirmOptions 
+} from './types';
