b221825a99cd8d8983b7851703353563977837be
DrJones <drjones@floor.io>
2025-04-02 08:56:42 +0200

    chore: move list constants to types.ts

--- src/components/list/index.ts
+++ src/components/list/index.ts
@@ -2,11 +2,20 @@
 export { default } from './list';
 export { default as createListItem } from './list-item';
 export {
+  // Types
   ListConfig,
   ListComponent,
   ListItemConfig,
   ListSectionConfig,
   ListType,
   ListLayout,
-  ListItemLayout
+  ListItemLayout,
+
+  // Constants
+  LIST_TYPES,
+  LIST_LAYOUTS,
+  LIST_ITEM_LAYOUTS,
+  LIST_CLASSES,
+  LIST_SCHEMA,
+  LIST_ITEM_STATES
 } from './types';
