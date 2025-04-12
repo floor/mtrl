50e1acfaae8fda26933f2701773e9710c2cb07fb
DrJones <drjones@floor.io>
2025-04-12 12:29:18 +0200

    feat: update drop down icon

--- src/components/select/features.ts
+++ src/components/select/features.ts
@@ -20,7 +20,7 @@ export const withTextfield = (config: SelectConfig) =>
     }
     
     // Create dropdown icon for the textfield
-    const dropdownIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>';
+    const dropdownIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 10l5 5 5-5H7z"/></svg>';
     
     // Create textfield component
     const textfield = createTextfield({
