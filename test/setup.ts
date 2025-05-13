8263fbbc74e544420cb646eca107d2a93de9e0cf
DrJones <drjones@floor.io>
2025-05-13 09:21:06 +0200

    fix(typescript): enhance test setup

--- test/setup.ts
+++ test/setup.ts
@@ -9,17 +9,24 @@ const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
   storageQuota: 10000000
 });
 
-// Make DOM globals available
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
-global.HTMLElement = dom.window.HTMLElement;
-global.Element = dom.window.Element;
-global.Node = dom.window.Node;
-global.Event = dom.window.Event;
-global.KeyboardEvent = dom.window.KeyboardEvent;
-global.MouseEvent = dom.window.MouseEvent;
-global.CustomEvent = dom.window.CustomEvent;
-global.DOMRect = dom.window.DOMRect;
+// Use type assertions to avoid TypeScript errors
+(global as any).window = dom.window;
+(global as any).document = dom.window.document;
+(global as any).navigator = dom.window.navigator;
+(global as any).HTMLElement = dom.window.HTMLElement;
+(global as any).Element = dom.window.Element;
+(global as any).Node = dom.window.Node;
+(global as any).Event = dom.window.Event;
+(global as any).KeyboardEvent = dom.window.KeyboardEvent;
+(global as any).MouseEvent = dom.window.MouseEvent;
+(global as any).CustomEvent = dom.window.CustomEvent;
+(global as any).DOMRect = dom.window.DOMRect;
+(global as any).HTMLButtonElement = dom.window.HTMLButtonElement;
+(global as any).HTMLInputElement = dom.window.HTMLInputElement;
+(global as any).HTMLDivElement = dom.window.HTMLDivElement;
+(global as any).Touch = dom.window.Touch;
+(global as any).TouchEvent = dom.window.TouchEvent;
+(global as any).DocumentFragment = dom.window.DocumentFragment;
+(global as any).DOMParser = dom.window.DOMParser;
 
 // Add any additional DOM-related setup if needed
